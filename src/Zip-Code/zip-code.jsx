e9b32f3f1a0c1ab3b08f2f5b67427e11c1dcea4d
cbommade <chandraprakashb15@gmail.com>
2022-05-02 23:01:22 +0530

    Zip code Input type updated

--- src/Zip-Code/zip-code.jsx
+++ src/Zip-Code/zip-code.jsx
@@ -12,7 +12,7 @@ export default function ZipCode() {
       <label for="auto-complete">AutoComplete</label>
       </div>
       <form id="zip-code" className="zip-code-form">
-        <input id="zip" className="zip-code-input" name="zip" type="text" pattern="[0-9]*" autoComplete={autoComplete?`on`:`off`} placeholder="Enter Zip Code" />
+        <input id="zip" className="zip-code-input" name="zip" type="number" pattern="[0-9]*" autoComplete={autoComplete?`on`:`off`} placeholder="Enter Zip Code" />
         <input className="submit-button" type="submit" value="Submit" />
       </form>
     </div>
